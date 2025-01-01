3610928648159fb775a313ddae2c673de1fc283d
nourlaabidi <nour.laabidi@supcom.tn>
2025-01-01 16:23:28 +0100

    add some changes to signup form

--- code/middleware/iam/src/main/webapp/js/signup.js
+++ code/middleware/iam/src/main/webapp/js/signup.js
@@ -24,15 +24,32 @@ document.getElementById("signup").onclick = function () {
             'Content-Type': 'application/json',
         },
         data: JSON.stringify(reqObj),
-        success: function(data) {
-            // Handle the success response from the server if needed
-            console.log('User created successfully.');
+        success: function (data) {
+            // Display a success message to the user
+            let successMessage = document.createElement('div');
+            successMessage.textContent = 'Account created successfully! ';
+            successMessage.style.color = 'green';
+            successMessage.style.marginTop = '15px';
+            document.querySelector('.form-signin').appendChild(successMessage);
+
+            // Redirect the user to the login page after 2 seconds
+            setTimeout(function () {
+                window.location.href = 'index.html';
+            }, 1000);
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             // Handle errors from the server
             console.error('Error creating user:', error);
             console.log(xhr.responseText);
+
+            // Display an error message to the user
+            let errorMessage = document.createElement('div');
+            errorMessage.textContent = 'Error creating account. Please try again.';
+            errorMessage.style.color = 'red';
+            errorMessage.style.marginTop = '15px';
+            document.querySelector('.form-signin').appendChild(errorMessage);
         },
+
         complete: function() {
             // This block will be executed regardless of success or failure
             console.log('Request completed.');
